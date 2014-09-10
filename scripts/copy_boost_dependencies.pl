a16d306d7c89112634b39b95586d253e38f459ef
Juraj Zikmund <jur.zikmund@gmail.com>
2014-09-11 00:18:42 +0200

    Copy boost dependencies perl script
    Travels through source code and copies only dependant boost sources

--- scripts/copy_boost_dependencies.pl
+++ scripts/copy_boost_dependencies.pl
@@ -3,47 +3,65 @@
 use strict; 
 use warnings;
 
-# Gotchangpdf test script #
-print "Running test script\n";
+use File::Path;
+use File::Copy;
+use File::Basename;
 
-# Directory containing test cases
-my $testdir = '../test';
+my $outdir = "..\\lib\\boost_1_55_0";
+my $srcdir = "..\\src";
+my $boostdir = "C:\\boost_1_55_0";
+my $bcp = "..\\lib\\boost_1_55_0\\tools\\bcp.exe";
 
-opendir (DIR, $src) or die $!;
-while (my $file = readdir(DIR)) {
-
-	# Path relative to this directory
-	my $file_path = "$testdir/$file";
-	
-    # We only want files
-    next unless (-f "$file_path");
-
-    # Use a regular expression to find files ending in .h
-    next unless ($file =~ m/\.h$/);
-	
-	    # Use a regular expression to find files ending in .h
-    next unless ($file =~ m/\.c$/);
-	
-	print $log "Found test case named \"$file\"...  ";
-	
-	system("$path $file_path > NUL");
-	
-	if ($? == 0) {
-		print $log "Passed!\n";
-	}
-	else {
-		print $log "Failed!\n";
-		$failed_count++;
-	}
+my @files = find_files($srcdir);
+foreach(@files) {
+    copy_file($_, $outdir);
 }
 
-if ($failed_count == 0) {
-	print "All tests have finished successfully";
-}
-else {
-	print "$failed_count test scenarios failed";
+sub copy_file {
+    my $source = $_[0];
+    my $dest = $_[1];
+
+	open (my $fh, "<", "$srcdir\\$source") || die $!;
+    while ( <$fh> ) {
+        if (/#.*include <(boost.*)>/) {
+			print "found $1\n";
+            system("$bcp --boost=$boostdir $1 $outdir") == 0 or die $!;
+        }
+    }
+    close($fh);
 }
 
-closedir(DIR);
-close($log);
-exit 0;
+sub find_files {
+    my $path = $_[0];
+    my @res;
+   
+    if (! -d $path) {
+        print "Input path is not a directory\n";
+        return @res;
+    }
+   
+    opendir(my $dh, $path) || die;   
+    while(readdir $dh) {
+        next if(/^\.{1,2}/);
+   
+        my $found = "$path\\$_";
+        print "$found\n";
+       
+        if (-d $found) {
+            push(@res, find_files($found));
+        }       
+        elsif (-f $found) {
+			# Use a regular expression to find files ending in .h or .c
+			next unless (/\.h|\.cpp$/);
+			
+            push(@res, $found);
+        }
+        else {
+            print "Unknown file type \"$found\"\n";
+        }
+    }
+   
+    closedir $dh;
+   
+    return @res;
+}
