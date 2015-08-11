d41a74fd5beecb5cc4bb67793b3bfab6be0e18f4
Juraj Zikmund <jur.zikmund@gmail.com>
2015-08-11 22:47:54 +0200

    Indirect object interface

--- src/gotchangpdf/implementation/c_indirect_object_reference.cpp
+++ src/gotchangpdf/implementation/c_indirect_object_reference.cpp
@@ -32,3 +32,25 @@ GOTCHANG_PDF_API error_type CALLING_CONVENTION IndirectReference_Release(Indirec
 	obj->Release();
 	return GOTCHANG_PDF_ERROR_SUCCES;
 }
+
+GOTCHANG_PDF_API error_type CALLING_CONVENTION IndirectReference_GetReferencedObjectNumber(IndirectReferenceHandle handle, out_integer_type result)
+{
+	IndirectObjectReference* obj = reinterpret_cast<IndirectObjectReference*>(handle);
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(obj);
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+	LOG_SCOPE(obj->GetFile()->GetFilename());
+
+	*result = obj->GetReferencedObjectNumber();
+	return GOTCHANG_PDF_ERROR_SUCCES;
+}
+
+GOTCHANG_PDF_API error_type CALLING_CONVENTION IndirectReference_GetReferencedGenerationNumber(IndirectReferenceHandle handle, out_integer_type result)
+{
+	IndirectObjectReference* obj = reinterpret_cast<IndirectObjectReference*>(handle);
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(obj);
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+	LOG_SCOPE(obj->GetFile()->GetFilename());
+
+	*result = obj->GetReferencedGenerationNumber();
+	return GOTCHANG_PDF_ERROR_SUCCES;
+}
