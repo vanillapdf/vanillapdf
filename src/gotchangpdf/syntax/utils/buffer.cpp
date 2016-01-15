9d2f333cc3c63a58a41210941a2070c9146f4531
Juraj Zikmund <jur.zikmund@gmail.com>
2016-01-15 19:15:16 +0100

    Buffer is finally inherited from vector

--- src/gotchangpdf/syntax/utils/buffer.cpp
+++ src/gotchangpdf/syntax/utils/buffer.cpp
@@ -5,53 +5,36 @@
 
 namespace gotchangpdf
 {
-	#pragma region Constructors
-
-	//Buffer::Buffer() {}
-	//Buffer::Buffer(BufferPtr other) : base_type(other) {}
-	//Buffer::Buffer(Buffer && other) : base_type(std::move(other)) {}
-	Buffer::Buffer(size_type count) : _value(count) {}
+	Buffer::Buffer(size_type count) : base_type(count) {}
 	Buffer::Buffer(const value_type * chars) : Buffer(chars, strlen(chars) + 1) {}
-	Buffer::Buffer(const value_type * chars, int len) : _value(&chars[0], &chars[len - 1]) { assert(_value.size() > 0); }
-	Buffer::Buffer(const value_type * begin, const value_type * end) : _value(begin, end) { assert(_value.size() > 0); }
-	Buffer::Buffer(size_type count, const value_type& val) : _value(count, val) {}
-	//Buffer::Buffer(const std::vector<ValueType>& data) : _value(data) { assert(_value.size() > 0); }
+	Buffer::Buffer(const value_type * chars, int len) : base_type(&chars[0], &chars[len - 1]) { assert(size() > 0); }
+	Buffer::Buffer(const value_type * begin, const value_type * end) : base_type(begin, end) { assert(size() > 0); }
+	Buffer::Buffer(size_type count, const value_type& val) : base_type(count, val) {}
 
-	#pragma endregion
-
-	#pragma region Operators
-	/*
-	bool Buffer::operator==(const IBuffer& other) const
+	bool Buffer::Equals(const Buffer& other) const
 	{
-		const Buffer *o = dynamic_cast<const Buffer*>(&other);
-		if (nullptr == o)
+		if (other.size() != size())
 			return false;
 
-		return _value == o->_value;
+		auto size = this->size();
+		for (decltype(size) i = 0; i < size; ++i) {
+			if (at(i) != other[i])
+				return false;
+		}
+
+		return true;
 	}
 
-	bool Buffer::operator<(const IBuffer& other) const
+	bool Buffer::operator<(const Buffer& other) const
 	{
-		const Buffer *o = dynamic_cast<const Buffer*>(&other);
-		if (nullptr == o)
-			return false;
+		auto size = this->size();
+		for (decltype(size) i = 0; i < size; ++i) {
+			if (at(i) == other[i])
+				continue;
 
-		return _value < o->_value;
+			return at(i) < other[i];
+		}
+
+		return false;
 	}
-	*/
-
-	//Buffer::ValueType Buffer::operator[](unsigned int i) const { return _value[i]; }
-
-	#pragma endregion
-
-	
-	//Buffer::ValueType Buffer::At(long at) const { return _value[at]; }
-	//void Buffer::Append(const Character& ch) { _value.push_back(ch); }
-	//void Buffer::Append(BufferPtr item) { Insert(0, item); }
-	//void Buffer::Insert(int idx, BufferPtr item) { _value.insert(_value.begin() + idx, item._value.begin(), item._value.end()); }
-	//int Buffer::Size() const { return _value.size(); }
-	//Buffer::ValueType* Buffer::Data() { return _value.data(); }
-	//const Buffer::ValueType* Buffer::Data() const { return _value.data(); }
-	//Buffer::ValueType* Buffer::Data(unsigned int idx) { return _value.data() + sizeof(ValueType) * idx; }
-	//const Buffer::ValueType* Buffer::Data(unsigned int idx) const { return _value.data() + sizeof(ValueType) * idx; }
 }
--- src/gotchangpdf/syntax/utils/buffer.h
+++ src/gotchangpdf/syntax/utils/buffer.h
@@ -10,20 +10,11 @@
 
 namespace gotchangpdf
 {
-	class Buffer : public IUnknown
+	class Buffer : public IUnknown, public std::vector<char>
 	{
 	public:
-		typedef std::vector<char> base_type;
-		typedef base_type::value_type value_type;
-		typedef base_type::size_type size_type;
-		typedef base_type::difference_type difference_type;
-		typedef base_type::pointer pointer;
-		typedef base_type::const_pointer const_pointer;
-		typedef base_type::reference reference;
-		typedef base_type::const_reference const_reference;
-
-		typedef base_type::iterator iterator;
-		typedef base_type::const_iterator const_iterator;
+		using base_type = std::vector<char>;
+		using base_type::base_type;
 
 	public:
 		Buffer() = default;
@@ -33,49 +24,16 @@ namespace gotchangpdf
 		Buffer(const value_type * begin, const value_type * end);
 		Buffer(size_type count, const value_type& val);
 
-		std::string ToString(void) const { return std::string(_value.begin(), _value.end()); }
+		std::string ToString(void) const { return std::string(begin(), end()); }
 		std::stringstream ToStringStream(void) const { return std::stringstream(ToString()); }
 
-		iterator begin() _NOEXCEPT { return _value.begin(); }
-		const_iterator begin() const _NOEXCEPT { return _value.begin(); }
-		iterator end() _NOEXCEPT { return _value.end(); }
-		const_iterator end() const _NOEXCEPT { return _value.end(); }
-		reference front() { return _value.front(); }
-		const_reference front() const { return _value.front(); }
+		bool Equals(const Buffer& other) const;
+		bool operator<(const Buffer& other) const;
 
-		iterator insert(const_iterator wher, const value_type& val) { return _value.insert(wher, val); }
-		void push_back(const value_type& val) { _value.push_back(val); }
-		void resize(size_type newsize) { _value.resize(newsize); }
-
-		size_type size() const _NOEXCEPT { return _value.size(); }
-		bool empty() const _NOEXCEPT { return _value.empty(); }
-		pointer data() _NOEXCEPT { return _value.data(); }
-		const_pointer data() const _NOEXCEPT { return _value.data(); }
-
-		reference operator[](size_type i) { return _value[i]; }
-		const_reference operator[](size_type i) const { return _value[i]; }
-
-		inline bool Equals(Buffer other) const { return _value == other._value; }
 		inline bool operator==(const Buffer& other) const { return Equals(other); }
 		inline bool operator!=(const Buffer& other) const { return !Equals(other); }
-		inline bool operator<(const Buffer& other) const { return _value < other._value; }
-
-		template<class _Iter>
-		typename std::enable_if<std::_Is_iterator<_Iter>::value, iterator>::type
-			insert(const_iterator wher, _Iter first, _Iter last) { return _value.insert(wher, first, last); }
-
-		//template<class _Iter,
-		//class = typename std::enable_if<std::_Is_iterator<_Iter>::value,
-		//	void>::type>
-		//	Buffer(_Iter begin, _Iter end) : _value(begin, end) {}
-
-		//template <typename U>
-		//Buffer(const std::vector<U> & other) : _Mybase(other.begin(), other.end()) {}
 
 		virtual ~Buffer() {}
-
-	private:
-		std::vector<value_type> _value;
 	};
 }
 
