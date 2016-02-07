3faa67ca4a3b29a1dcbf5f4d263b2fb5c6142353
Juraj Zikmund <jur.zikmund@gmail.com>
2016-02-07 20:14:02 +0100

    Content stream parsing moved to syntax

---
+++ src/gotchangpdf/syntax/parsers/contents/content_stream_objects.h
@@ -0,0 +1,62 @@
+#ifndef _CONTENT_STREAM_OBJECTS_H
+#define _CONTENT_STREAM_OBJECTS_H
+
+#include "syntax_fwd.h"
+#include "constants.h"
+#include "content_stream_operation_base.h"
+
+#include <vector>
+
+namespace gotchangpdf
+{
+	namespace syntax
+	{
+		namespace contents
+		{
+			class ContentObjectBase : public syntax::contents::InstructionBase
+			{
+			public:
+				enum class Type
+				{
+					TextObject,
+					InlineImageObject
+				};
+
+				virtual Type GetType(void) const _NOEXCEPT = 0;
+				virtual syntax::contents::InstructionBase::Type GetInstructionType(void) const _NOEXCEPT override { return InstructionBase::Type::Object; }
+			};
+
+			class TextObject : public ContentObjectBase
+			{
+			public:
+				TextObject(contents::BaseOperationCollection ops) : _operations(ops) {}
+
+				virtual Type GetType(void) const _NOEXCEPT override { return Type::TextObject; }
+
+				types::uinteger GetOperationsSize(void) const { return _operations.size(); }
+				OperationBasePtr GetOperationAt(types::uinteger at) const { return _operations.at(at); }
+
+			private:
+				BaseOperationCollection _operations;
+			};
+
+			class InlineImageObject : public ContentObjectBase
+			{
+			public:
+				InlineImageObject(DictionaryObjectPtr dictionary, BufferPtr data)
+					: m_dictionary(dictionary), m_data(data) {}
+
+				virtual Type GetType(void) const _NOEXCEPT override { return Type::InlineImageObject; }
+
+				DictionaryObjectPtr GetDictionary() const { return m_dictionary; }
+				BufferPtr GetData() const { return m_data; }
+
+			private:
+				DictionaryObjectPtr m_dictionary;
+				BufferPtr m_data;
+			};
+		}
+	}
+}
+
+#endif /* _CONTENT_STREAM_OBJECTS_H */
---
+++ src/gotchangpdf/syntax/parsers/contents/content_stream_operations.h
@@ -0,0 +1,101 @@
+#ifndef _CONTENT_STREAM_OPERATIONS_H
+#define _CONTENT_STREAM_OPERATIONS_H
+
+#include "syntax_fwd.h"
+#include "content_stream_operation_base.h"
+#include "string_object.h"
+
+#include <vector>
+
+namespace gotchangpdf
+{
+	namespace syntax
+	{
+		namespace contents
+		{
+			class OperationBeginText : public OperationBase
+			{
+			public:
+				explicit OperationBeginText(const std::vector<ObjectPtr>& operands)
+				{
+					assert(operands.size() == 0);
+				}
+
+				virtual Type GetOperationType(void) const _NOEXCEPT override { return Type::BeginText; }
+			};
+
+			class OperationEndText : public OperationBase
+			{
+			public:
+				explicit OperationEndText(const std::vector<ObjectPtr>& operands)
+				{
+					assert(operands.size() == 0);
+				}
+
+				virtual Type GetOperationType(void) const _NOEXCEPT override { return Type::EndText; }
+			};
+
+			class OperationTextShow : public OperationBase
+			{
+			public:
+				explicit OperationTextShow(const std::vector<ObjectPtr>& operands)
+				{
+					if (1 != operands.size()) {
+						assert(!"Text show operation has invalid arguments");
+						return;
+					}
+
+					auto item = operands.at(0);
+					if (!syntax::ObjectUtils::IsType<syntax::StringObjectPtr>(item)) {
+						assert(!"Text show operation has invalid arguments");
+						return;
+					}
+
+					_str = syntax::ObjectUtils::ConvertTo<syntax::StringObjectPtr>(item);
+				}
+
+				virtual Type GetOperationType(void) const _NOEXCEPT override { return Type::TextShow; }
+
+				StringObjectPtr Value() const { return _str; }
+
+			private:
+				StringObjectPtr _str;
+			};
+
+			class OperationBeginInlineImageObject : public OperationBase
+			{
+			public:
+				explicit OperationBeginInlineImageObject(const std::vector<ObjectPtr>& operands)
+				{
+					assert(operands.size() == 0);
+				}
+
+				virtual Type GetOperationType(void) const _NOEXCEPT override { return Type::BeginInlineImageObject; }
+			};
+
+			class OperationBeginInlineImageData : public OperationBase
+			{
+			public:
+				explicit OperationBeginInlineImageData(const std::vector<ObjectPtr>& operands)
+				{
+					assert(operands.size() == 0);
+				}
+
+				virtual Type GetOperationType(void) const _NOEXCEPT override { return Type::BeginInlineImageData; }
+			};
+
+			class OperationEndInlineImageObject : public OperationBase
+			{
+			public:
+				explicit OperationEndInlineImageObject(const std::vector<ObjectPtr>& operands)
+				{
+					assert(operands.size() == 0);
+				}
+
+				virtual Type GetOperationType(void) const _NOEXCEPT override { return Type::EndInlineImageObject; }
+			};
+		}
+	}
+}
+
+#endif /* _CONTENT_STREAM_OPERATIONS_H */
