3cb16e823807c66fe59ac67f9ab836daca6d5512
Juraj Zikmund <jur.zikmund@gmail.com>
2016-05-10 00:59:42 +0200

    Initial python testing script

---
+++ scripts/run_test_debug.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import platform
+import datetime
+import os
+import subprocess
+import sys
+
+print ('Running test script')
+
+path = ""
+if len(sys.argv) < 2:
+	print ("No mode was specified (debug/release)")
+	sys.exit(1)
+	
+if sys.argv[1] == "debug":
+	path = '..\\build\\gotchangpdf.test\\debug\\gotchangpdf.test.exe'
+elif sys.argv[1] == "release":
+	path = '..\\build\\gotchangpdf.test\\release\\gotchangpdf.test.exe'
+else:
+	print ("Incorrect mode was specified (debug/release)")
+	sys.exit(1)
+
+testdir = '..\\test\\'
+
+system = platform.system()
+now = datetime.datetime.now()
+logpath = '..\\log\\{}-{}-{}_{}-{}-{}_{}.log'.format(now.day, now.month, now.year, now.hour, now.minute, now.second, system)
+
+# Open log file
+logfile = open(logpath, 'w')
+FNULL = open(os.devnull, 'w')
+failed_count = 0;
+
+for root, dirs, files in os.walk(testdir):
+	for file in files:
+		if not file.endswith(".pdf"):
+			continue
+			
+		print ("Found test case named {}...  ".format(file))
+		logfile.write("Found test case named {}...  ".format(file))
+		
+		file_path = os.path.join(root, file)
+
+		rv = subprocess.call([path, file_path], stdout=FNULL)
+		if (rv == 0):
+			print ("Passed!")
+			logfile.write("Passed!\n")
+		else:
+			print ("Failed!")
+			logfile.write("Failed!\n")
+			failed_count += 1
+		
+if (failed_count == 0):
+	print ("All tests have finished successfully");
+	logfile.write("All tests have finished successfully")
+else:
+	print ("{} test scenarios failed".format(failed_count));
+	logfile.write("{} test scenarios failed".format(failed_count))
+			
+logfile.close()
+sys.exit(0)
