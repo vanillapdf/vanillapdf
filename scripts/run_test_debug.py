5c41bd64ae78d40f3b716e5e2fe448c0dbb2600f
Juraj Zikmund <jur.zikmund@gmail.com>
2016-05-12 10:34:36 +0200

    Encrypted files can be configured with user and owner password as well

--- scripts/run_test_debug.py
+++ scripts/run_test_debug.py
@@ -7,32 +7,45 @@ import subprocess
 import sys
 import json
 
-PASSWORD_KEY = "password"
+USER_PASSWORD_KEY = "user_password"
+OWNER_PASSWORD_KEY = "owner_password"
 PASSWORD_OPTION = "-p"
 CERTIFICATE_KEY = "certificate"
 CERTIFICATE_OPTION = "-k"
 
+failed_count = 0;
+def process_rv( rv ):
+	global failed_count
+	
+	if (rv == 0):
+		print ("Passed!")
+		logfile.write("Passed!\n")
+	else:
+		print ("Failed!")
+		logfile.write("Failed!\n")
+		failed_count += 1
+
+#Inform the user that process has started
 print ('Running test script')
 
+# Use default release configuration
 path = ""
-if len(sys.argv) < 2:
-	print ("No mode was specified (debug/release)")
-	sys.exit(1)
-	
-if sys.argv[1] == "debug":
-	path = '..\\build\\gotchangpdf.test\\debug\\gotchangpdf.test.exe'
-elif sys.argv[1] == "release":
+if (len(sys.argv) < 2 or sys.argv[1] == "release"):
 	path = '..\\build\\gotchangpdf.test\\release\\gotchangpdf.test.exe'
+elif sys.argv[1] == "debug":
+	path = '..\\build\\gotchangpdf.test\\debug\\gotchangpdf.test.exe'
 else:
 	print ("Incorrect mode was specified (debug/release)")
 	sys.exit(1)
 
+# All of the test files shall be in the test folder
 testdir = '..\\test\\'
 
 system = platform.system()
 now = datetime.datetime.now()
 logpath = '..\\log\\{}-{}-{}_{}-{}-{}_{}.log'.format(now.day, now.month, now.year, now.hour, now.minute, now.second, system)
 
+# Open the settings for encrypted files
 encryption_data = ""
 with open('encryption.cfg') as encryption_config:    
 	encryption_data = json.load(encryption_config)
@@ -40,41 +53,62 @@ with open('encryption.cfg') as encryption_config:
 # Open log file
 logfile = open(logpath, 'w')
 FNULL = open(os.devnull, 'w')
-failed_count = 0;
 
+# Enumerate all test files
 for root, dirs, files in os.walk(testdir):
 	for file in files:
+		# Skip files that does not end with .pdf
 		if not file.endswith(".pdf"):
 			continue
 			
 		print ("Found test case named {}...  ".format(file))
 		logfile.write("Found test case named {}...  ".format(file))
 		
+		# Calculate full file path
 		file_path = os.path.join(root, file)
 		is_encrypted = file in encryption_data
 		rv = -1
+		
+		# Check if filename is in our encrypted configuration file
 		if (is_encrypted):
-			if (PASSWORD_KEY in encryption_data[file]):
-				password = encryption_data[file][PASSWORD_KEY]
-				rv = subprocess.call([path, file_path, PASSWORD_OPTION, password], stdout=FNULL)
-			elif (CERTIFICATE_KEY in encryption_data[file]):
+			# Authentication using user or owner password
+			if (USER_PASSWORD_KEY in encryption_data[file] or OWNER_PASSWORD_KEY in encryption_data[file]):
+				# User password
+				if (USER_PASSWORD_KEY in encryption_data[file]):
+					logfile.write(os.linesep)
+					logfile.write("Using user password...  ")
+					user_password = encryption_data[file][USER_PASSWORD_KEY]
+					rv = subprocess.call([path, file_path, PASSWORD_OPTION, user_password], stdout=FNULL)
+					process_rv(rv)
+					
+				# Owner password
+				if (OWNER_PASSWORD_KEY in encryption_data[file]):
+					logfile.write(os.linesep)
+					logfile.write("Using owner password...  ")
+					owner_password = encryption_data[file][OWNER_PASSWORD_KEY]
+					rv = subprocess.call([path, file_path, PASSWORD_OPTION, owner_password], stdout=FNULL)
+					process_rv(rv)
+					
+				continue
+					
+			# Authentication using certificate
+			if (CERTIFICATE_KEY in encryption_data[file]):
+				logfile.write(os.linesep)
+				logfile.write("Using certificate key...  ")
 				key = encryption_data[file][CERTIFICATE_KEY]
 				rv = subprocess.call([path, file_path, CERTIFICATE_OPTION, key], stdout=FNULL)
-			else:
-				print ("Configuration error for file {}".format(file))
-				logfile.write("Configuration error for file {}".format(file))
-				continue
-		else:
-			rv = subprocess.call([path, file_path], stdout=FNULL)
+				process_rv(rv)
+				continue;
+				
+			print ("Configuration error for file {}".format(file))
+			logfile.write("Configuration error for file {}".format(file))
+			continue
 			
-		if (rv == 0):
-			print ("Passed!")
-			logfile.write("Passed!\n")
-		else:
-			print ("Failed!")
-			logfile.write("Failed!\n")
-			failed_count += 1
-		
+		# Run test with default behavior
+		rv = subprocess.call([path, file_path], stdout=FNULL)
+		process_rv(rv)
+	
+# Log if everything has finished successfully
 if (failed_count == 0):
 	print ("All tests have finished successfully");
 	logfile.write("All tests have finished successfully")
@@ -82,5 +116,8 @@ else:
 	print ("{} test scenarios failed".format(failed_count));
 	logfile.write("{} test scenarios failed".format(failed_count))
 			
+# Cleanup
 logfile.close()
+
+# Return success
 sys.exit(0)
