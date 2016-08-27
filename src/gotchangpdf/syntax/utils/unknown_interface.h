67cd1b0753419b482ef9be4ba80165f86c25eeb2
Juraj Zikmund <jur.zikmund@gmail.com>
2016-08-27 20:37:15 +0200

    Weak reference implementation

--- src/gotchangpdf/syntax/utils/unknown_interface.h
+++ src/gotchangpdf/syntax/utils/unknown_interface.h
@@ -5,25 +5,79 @@
 
 namespace gotchangpdf
 {
+	class IUnknown;
+
+	class WeakReferenceCounter
+	{
+	public:
+		WeakReferenceCounter(IUnknown* ptr) noexcept : m_active(true), m_reference(ptr) { assert(nullptr != ptr); }
+		WeakReferenceCounter(const WeakReferenceCounter&) = delete;
+		WeakReferenceCounter(WeakReferenceCounter&&) = delete;
+		WeakReferenceCounter& operator= (const WeakReferenceCounter&) = delete;
+		WeakReferenceCounter& operator= (WeakReferenceCounter&&) = delete;
+
+		bool IsActive() const noexcept { return m_active.load(); }
+		void Deactivate() noexcept { m_active = false; }
+		IUnknown* GetReference() const noexcept { return m_reference; }
+
+	private:
+		std::atomic_bool m_active = true;
+		IUnknown* m_reference;
+	};
+
+	template <typename T>
+	class WeakReference
+	{
+	public:
+		static_assert(std::is_base_of<IUnknown, T>::value,
+			"WeakReference<T>: T must be derived from IUnknown");
+
+	public:
+		WeakReference() = default;
+		WeakReference(std::shared_ptr<WeakReferenceCounter> ptr) : m_ptr(ptr)
+		{
+			// Verify type correctness
+			assert(nullptr != static_cast<T*>(m_ptr->GetReference()));
+		}
+
+		bool IsEmpty() const noexcept { return (nullptr == m_ptr); }
+		bool IsActive() const { return m_ptr ? m_ptr->IsActive() : false; }
+
+		T* GetReference() const
+		{
+			assert(m_ptr && "Referenced pointer is empty");
+			return m_ptr ? static_cast<T*>(m_ptr->GetReference()) : nullptr;
+		}
+
+	private:
+		std::shared_ptr<WeakReferenceCounter> m_ptr;
+	};
+
 	class IUnknown
 	{
 	public:
-		IUnknown() noexcept : m_ref_counter(0) {}
-		IUnknown(const IUnknown&) noexcept : m_ref_counter(0) {}
-		IUnknown& operator= (const IUnknown&) noexcept { return *this; }
+		IUnknown() noexcept : m_ref_counter(0) { m_weak_ref = std::make_shared<WeakReferenceCounter>(this); }
+		IUnknown(const IUnknown&) noexcept : m_ref_counter(0) { m_weak_ref = std::make_shared<WeakReferenceCounter>(this); }
+		IUnknown& operator= (const IUnknown&) noexcept { m_weak_ref = std::make_shared<WeakReferenceCounter>(this); return *this; }
+
+		template <typename T>
+		WeakReference<T> GetWeakReference() { return WeakReference<T>(m_weak_ref); }
 
 		uint32_t UseCount() const noexcept { return m_ref_counter.load(); }
-		void AddRef() const noexcept { m_ref_counter++; }
-		void Release() const noexcept
+		void AddRef() noexcept { m_ref_counter++; }
+		void Release() noexcept
 		{
-			if (--m_ref_counter == 0)
+			if (--m_ref_counter == 0) {
+				m_weak_ref->Deactivate();
 				delete this;
+			}
 		}
 
 		virtual ~IUnknown() = 0;
 
 	private:
-		mutable std::atomic_uint32_t m_ref_counter = 0;
+		std::atomic_uint32_t m_ref_counter = 0;
+		std::shared_ptr<WeakReferenceCounter> m_weak_ref;
 	};
 
 	inline IUnknown::~IUnknown() {}
