58c3437e1119d3c6f61f82fbc02c4c852b6ff0f6
Juraj Zikmund <jur.zikmund@gmail.com>
2016-09-22 15:08:10 +0200

    Flate encoding fix
    Predictor now support streams

--- src/gotchangpdf/syntax/filters/flate_decode_filter.cpp
+++ src/gotchangpdf/syntax/filters/flate_decode_filter.cpp
@@ -12,8 +12,7 @@ namespace gotchangpdf
 	{
 		BufferPtr FlateDecodeFilter::Encode(BufferPtr src, DictionaryObjectPtr parameters) const
 		{
-			auto result = ZlibWrapper::Deflate(src);
-			return ApplyPredictor(result, parameters);
+			return ZlibWrapper::Deflate(src);
 		}
 
 		BufferPtr FlateDecodeFilter::Decode(BufferPtr src, DictionaryObjectPtr parameters) const
@@ -24,8 +23,7 @@ namespace gotchangpdf
 
 		BufferPtr FlateDecodeFilter::Encode(std::istream& src, types::stream_size length, DictionaryObjectPtr parameters) const
 		{
-			auto result = ZlibWrapper::Deflate(src, length);
-			return ApplyPredictor(result, parameters);
+			return ZlibWrapper::Deflate(src, length);
 		}
 
 		BufferPtr FlateDecodeFilter::Decode(std::istream& src, types::stream_size length, DictionaryObjectPtr parameters) const
@@ -36,8 +34,12 @@ namespace gotchangpdf
 
 		BufferPtr FlateDecodeFilter::ApplyPredictor(BufferPtr src, DictionaryObjectPtr parameters) const
 		{
-			BufferPtr result;
+			auto stream = src->ToStringStream();
+			return ApplyPredictor(stream, src->size(), parameters);
+		}
 
+		BufferPtr FlateDecodeFilter::ApplyPredictor(std::istream& src, types::stream_size length, DictionaryObjectPtr parameters) const
+		{
 			IntegerObjectPtr predictor = 1;
 			if (parameters->Contains(constant::Name::Predictor)) {
 				predictor = parameters->FindAs<IntegerObjectPtr>(constant::Name::Predictor);
@@ -46,7 +48,8 @@ namespace gotchangpdf
 
 			// No prediction was used
 			if (predictor == 1) {
-				return src;
+				auto length_converted = ValueConvertUtils::SafeConvert<size_t>(length);
+				return Stream(src).read(length_converted);
 			}
 
 			IntegerObjectPtr colors = 1;
@@ -71,7 +74,8 @@ namespace gotchangpdf
 
 			if (*predictor == 2) {
 				throw NotSupportedException("TIFF predictor is currently not supported");
-			} else if (*predictor >= 10) {
+			}
+			else if (*predictor >= 10) {
 				types::native_int bytesPerPixel = colors->SafeConvert<types::native_int>() * bits->SafeConvert<types::native_int>() / 8;
 				types::native_int bytesPerRow = (
 					colors->SafeConvert<types::native_int>()
@@ -80,8 +84,8 @@ namespace gotchangpdf
 					+ 7
 					) / 8;
 
-				auto tmp = src->ToStringStream();
-				Stream strm(tmp);
+				BufferPtr result;
+				Stream strm(src);
 				Buffer curr(bytesPerRow, '\0');
 				Buffer prior(bytesPerRow, '\0');
 
@@ -90,63 +94,67 @@ namespace gotchangpdf
 					strm.read(curr, bytesPerRow);
 
 					switch (filter) {
-						case 0: //PNG_FILTER_NONE
-							break;
-						case 1: //PNG_FILTER_SUB
-							for (types::native_int i = bytesPerPixel; i < bytesPerRow; i++) {
-								curr[i] += curr[i - bytesPerPixel];
-							}
-							break;
-						case 2: //PNG_FILTER_UP
-							for (types::native_int i = 0; i < bytesPerRow; i++) {
-								curr[i] += prior[i];
-							}
-							break;
-						case 3: //PNG_FILTER_AVERAGE
-							for (types::native_int i = 0; i < bytesPerPixel; i++) {
-								curr[i] += (prior[i] / 2);
-							}
-							for (types::native_int i = bytesPerPixel; i < bytesPerRow; i++) {
-								curr[i] += (((curr[i - bytesPerPixel] & 0xff) + (prior[i] & 0xff)) / 2);
-							}
-							break;
-						case 4: //PNG_FILTER_PAETH
-							for (types::native_int i = 0; i < bytesPerPixel; i++) {
-								curr[i] += prior[i];
-							}
+					case 0: //PNG_FILTER_NONE
+						break;
+					case 1: //PNG_FILTER_SUB
+						for (types::native_int i = bytesPerPixel; i < bytesPerRow; i++) {
+							curr[i] += curr[i - bytesPerPixel];
+						}
+						break;
+					case 2: //PNG_FILTER_UP
+						for (types::native_int i = 0; i < bytesPerRow; i++) {
+							curr[i] += prior[i];
+						}
+						break;
+					case 3: //PNG_FILTER_AVERAGE
+						for (types::native_int i = 0; i < bytesPerPixel; i++) {
+							curr[i] += (prior[i] / 2);
+						}
+						for (types::native_int i = bytesPerPixel; i < bytesPerRow; i++) {
+							curr[i] += (((curr[i - bytesPerPixel] & 0xff) + (prior[i] & 0xff)) / 2);
+						}
+						break;
+					case 4: //PNG_FILTER_PAETH
+						for (types::native_int i = 0; i < bytesPerPixel; i++) {
+							curr[i] += prior[i];
+						}
 
-							for (types::native_int i = bytesPerPixel; i < bytesPerRow; i++) {
-								uint8_t a = curr[i - bytesPerPixel] & 0xff;
-								uint8_t b = prior[i] & 0xff;
-								uint8_t c = prior[i - bytesPerPixel] & 0xff;
+						for (types::native_int i = bytesPerPixel; i < bytesPerRow; i++) {
+							uint8_t a = curr[i - bytesPerPixel] & 0xff;
+							uint8_t b = prior[i] & 0xff;
+							uint8_t c = prior[i - bytesPerPixel] & 0xff;
 
-								int p = a + b - c;
-								int pa = std::abs(p - a);
-								int pb = std::abs(p - b);
-								int pc = std::abs(p - c);
+							int p = a + b - c;
+							int pa = std::abs(p - a);
+							int pb = std::abs(p - b);
+							int pc = std::abs(p - c);
 
-								uint8_t ret;
-								if ((pa <= pb) && (pa <= pc)) {
-									ret = a;
-								} else if (pb <= pc) {
-									ret = b;
-								} else {
-									ret = c;
-								}
-								curr[i] += ret;
+							uint8_t ret;
+							if ((pa <= pb) && (pa <= pc)) {
+								ret = a;
 							}
-							break;
-						default:
-							LOG_ERROR_GLOBAL << "Unknown filter type: " << filter;
-							break;
+							else if (pb <= pc) {
+								ret = b;
+							}
+							else {
+								ret = c;
+							}
+							curr[i] += ret;
+						}
+						break;
+					default:
+						LOG_ERROR_GLOBAL << "Unknown filter type: " << filter;
+						break;
 					}
 
 					result->insert(result.end(), curr.begin(), curr.end());
 					std::swap(curr, prior);
 				}
+
+				return result;
 			}
 
-			return result;
+			throw GeneralException("Unknown predictor type");
 		}
 	}
 }
