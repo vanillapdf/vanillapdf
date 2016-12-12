0b047885281ea05a4d92f964408f62b16a7cefb6
Juraj Zikmund <jur.zikmund@gmail.com>
2016-12-12 22:57:25 +0100

    Indirect reference GetReferencedObjectAs fix
    Deferring resolution using template tricks

--- src/gotchangpdf/syntax/objects/indirect_object_reference.h
+++ src/gotchangpdf/syntax/objects/indirect_object_reference.h
@@ -20,10 +20,9 @@ namespace gotchangpdf
 			ObjectPtr operator->() const { return GetReferencedObject(); }
 
 			template <typename T>
-			const T GetReferencedObjectAs() const
+			T GetReferencedObjectAs() const
 			{
-				auto direct = GetReferencedObject();
-				return ObjectUtils::ConvertTo<T>(direct);
+				return InternalGetReferencedObjectAs<T>();
 			}
 
 			virtual Object::Type GetType(void) const noexcept override { return Object::Type::IndirectReference; }
@@ -61,6 +60,34 @@ namespace gotchangpdf
 			mutable types::big_uint m_reference_object_number = 0;
 			mutable types::ushort m_reference_generation_number = 0;
 			mutable WeakReference<Object> m_reference;
+
+			template <
+				typename T,
+
+				// Lets talk about templates
+				// The original code looked like this:
+
+				// return ObjectUtils::ConvertTo<T>(direct);
+
+				// This unfortunately does not work on gcc.
+				// Problem is that class ObjectUtils requires
+				// definition of IndirectObjectReference,
+				// therefore this class is not yet defined.
+				// To work around this problem I've passed
+				// another defauled template parameter U
+				// to defer resolution of class ObjectUtils.
+				// That even was not enough, because the
+				// compiler does not know, that this type
+				// contains static templated method ConvertTo.
+				// I had to explictitly specify this using
+				// U::template syntax.
+				typename U = ObjectUtils
+			>
+			T InternalGetReferencedObjectAs() const
+			{
+				auto direct = GetReferencedObject();
+				return U::template ConvertTo<T>(direct);
+			}
 		};
 	}
 }
