886d67a2a6837c18934db39a4804f52e2dfac399
Juraj Zikmund <jur.zikmund@gmail.com>
2016-12-15 00:08:41 +0100

    Missing header

--- src/gotchangpdf/syntax/utils/buffer.cpp
+++ src/gotchangpdf/syntax/utils/buffer.cpp
@@ -1,10 +1,12 @@
 #include "precompiled.h"
 #include "buffer.h"
 
+#include <cstring>
+
 namespace gotchangpdf
 {
 	Buffer::Buffer(size_type count) : m_data(count) {}
-	Buffer::Buffer(const char * chars) : Buffer(chars, strlen(chars)) {}
+	Buffer::Buffer(const char * chars) : Buffer(chars, std::strlen(chars)) {}
 	Buffer::Buffer(const value_type * begin, const value_type * end) : m_data(begin, end) { assert(m_data.size() > 0); }
 	Buffer::Buffer(size_type count, const value_type& val) : m_data(count, val) {}
 
@@ -21,25 +23,25 @@ namespace gotchangpdf
 	bool operator==(const char * left, const Buffer& right)
 	{
 		assert(nullptr != left);
-		return (0 == strncmp(left, reinterpret_cast<const char*>(right.data()), right.size()));
+		return (0 == std::strncmp(left, reinterpret_cast<const char*>(right.data()), right.size()));
 	}
 
 	bool operator==(const Buffer& left, const char * right)
 	{
 		assert(nullptr != right);
-		return (0 == strncmp(right, reinterpret_cast<const char*>(left.data()), left.size()));
+		return (0 == std::strncmp(right, reinterpret_cast<const char*>(left.data()), left.size()));
 	}
 
 	bool operator!=(const char * left, const Buffer& right)
 	{
 		assert(nullptr != left);
-		return (0 != strncmp(left, reinterpret_cast<const char*>(right.data()), right.size()));
+		return (0 != std::strncmp(left, reinterpret_cast<const char*>(right.data()), right.size()));
 	}
 
 	bool operator!=(const Buffer& left, const char * right)
 	{
 		assert(nullptr != right);
-		return (0 != strncmp(right, reinterpret_cast<const char*>(left.data()), left.size()));
+		return (0 != std::strncmp(right, reinterpret_cast<const char*>(left.data()), left.size()));
 	}
 
 	std::ostream& operator<<(std::ostream& os, const Buffer& value)
