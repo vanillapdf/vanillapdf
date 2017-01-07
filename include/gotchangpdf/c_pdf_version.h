20e7ed9c975fed46079e55c87e6e28cb4cedf555
Juraj Zikmund <jur.zikmund@gmail.com>
2017-01-07 21:53:29 +0100

    PDF version reference

--- include/gotchangpdf/c_pdf_version.h
+++ include/gotchangpdf/c_pdf_version.h
@@ -10,17 +10,61 @@ extern "C"
 {
 #endif
 
+	/**
+	* \file c_pdf_version.h
+	* This file contains class definitions for PDFVersion
+	*/
+
+	/**
+	* \brief Range of available PDF versions
+	*/
 	typedef enum
 	{
+		/**
+		* \brief PDF version 1.0
+		*/
 		PDFVersion_10 = 0,
+
+		/**
+		* \brief PDF version 1.1
+		*/
 		PDFVersion_11 = 1,
+
+		/**
+		* \brief PDF version 1.2
+		*/
 		PDFVersion_12 = 2,
+
+		/**
+		* \brief PDF version 1.3
+		*/
 		PDFVersion_13 = 3,
+
+		/**
+		* \brief PDF version 1.4
+		*/
 		PDFVersion_14 = 4,
+
+		/**
+		* \brief PDF version 1.5
+		*/
 		PDFVersion_15 = 5,
+
+		/**
+		* \brief PDF version 1.6
+		*/
 		PDFVersion_16 = 6,
+
+		/**
+		* \brief PDF version 1.7
+		*/
 		PDFVersion_17 = 7
-	} PDFVersion, *PPDFVersion;
+	} PDFVersion;
+
+	/**
+	* \brief Pointer to PDFVersion
+	*/
+	typedef PDFVersion *PPDFVersion;
 
 #ifdef __cplusplus
 };
