3d3720a4db0edbdd3e8c2ed310f503e0d99b50ef
Juraj Zikmund <jur.zikmund@gmail.com>
2017-01-11 18:58:38 +0100

    Renamed font map params

--- src/gotchangpdf/implementation/c_font_map.cpp
+++ src/gotchangpdf/implementation/c_font_map.cpp
@@ -8,32 +8,32 @@ using namespace gotchangpdf;
 using namespace gotchangpdf::syntax;
 using namespace gotchangpdf::semantics;
 
-GOTCHANG_PDF_API error_type CALLING_CONVENTION FontMap_Contains(FontMapHandle handle, NameObjectHandle name_handle, out_boolean_type result)
+GOTCHANG_PDF_API error_type CALLING_CONVENTION FontMap_Contains(FontMapHandle handle, NameObjectHandle key_handle, out_boolean_type result)
 {
 	FontMap* obj = reinterpret_cast<FontMap*>(handle);
-	NameObject* name = reinterpret_cast<NameObject*>(name_handle);
+	NameObject* key = reinterpret_cast<NameObject*>(key_handle);
 	RETURN_ERROR_PARAM_VALUE_IF_NULL(obj);
-	RETURN_ERROR_PARAM_VALUE_IF_NULL(name);
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(key);
 	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
 
 	try
 	{
-		*result = obj->Contains(name);
+		*result = obj->Contains(key);
 		return GOTCHANG_PDF_ERROR_SUCCES;
 	} CATCH_GOTCHNGPDF_EXCEPTIONS
 }
 
-GOTCHANG_PDF_API error_type CALLING_CONVENTION FontMap_Find(FontMapHandle handle, NameObjectHandle name_handle, PFontHandle result)
+GOTCHANG_PDF_API error_type CALLING_CONVENTION FontMap_Find(FontMapHandle handle, NameObjectHandle key_handle, PFontHandle result)
 {
 	FontMap* obj = reinterpret_cast<FontMap*>(handle);
-	NameObject* name = reinterpret_cast<NameObject*>(name_handle);
+	NameObject* key = reinterpret_cast<NameObject*>(key_handle);
 	RETURN_ERROR_PARAM_VALUE_IF_NULL(obj);
-	RETURN_ERROR_PARAM_VALUE_IF_NULL(name);
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(key);
 	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
 
 	try
 	{
-		auto direct = obj->Find(name);
+		auto direct = obj->Find(key);
 		auto ptr = direct.AddRefGet();
 		*result = reinterpret_cast<FontHandle>(ptr);
 		return GOTCHANG_PDF_ERROR_SUCCES;
