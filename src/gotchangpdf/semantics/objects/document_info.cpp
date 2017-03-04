d27e0dbd9cf3369b073adb34c84af82c4699ed2e
Juraj Zikmund <jur.zikmund@gmail.com>
2017-03-04 23:15:24 +0100

    Formatting

--- src/gotchangpdf/semantics/objects/document_info.cpp
+++ src/gotchangpdf/semantics/objects/document_info.cpp
@@ -9,56 +9,63 @@ DocumentInfo::DocumentInfo(syntax::DictionaryObjectPtr root) : HighLevelObject(r
 }
 
 bool DocumentInfo::Title(syntax::OutputStringObjectPtr& result) const {
-	if (!_obj->Contains(constant::Name::Title))
+	if (!_obj->Contains(constant::Name::Title)) {
 		return false;
+	}
 
 	result = _obj->FindAs<syntax::StringObjectPtr>(constant::Name::Title);
 	return true;
 }
 
 bool DocumentInfo::Author(syntax::OutputStringObjectPtr& result) const {
-	if (!_obj->Contains(constant::Name::Author))
+	if (!_obj->Contains(constant::Name::Author)) {
 		return false;
+	}
 
 	result = _obj->FindAs<syntax::StringObjectPtr>(constant::Name::Author);
 	return true;
 }
 
 bool DocumentInfo::Subject(syntax::OutputStringObjectPtr& result) const {
-	if (!_obj->Contains(constant::Name::Subject))
+	if (!_obj->Contains(constant::Name::Subject)) {
 		return false;
+	}
 
 	result = _obj->FindAs<syntax::StringObjectPtr>(constant::Name::Subject);
 	return true;
 }
 
 bool DocumentInfo::Keywords(syntax::OutputStringObjectPtr& result) const {
-	if (!_obj->Contains(constant::Name::Keywords))
+	if (!_obj->Contains(constant::Name::Keywords)) {
 		return false;
+	}
 
 	result = _obj->FindAs<syntax::StringObjectPtr>(constant::Name::Keywords);
 	return true;
 }
 
 bool DocumentInfo::Creator(syntax::OutputStringObjectPtr& result) const {
-	if (!_obj->Contains(constant::Name::Creator))
+	if (!_obj->Contains(constant::Name::Creator)) {
 		return false;
+	}
 
 	result = _obj->FindAs<syntax::StringObjectPtr>(constant::Name::Creator);
 	return true;
 }
 
 bool DocumentInfo::Producer(syntax::OutputStringObjectPtr& result) const {
-	if (!_obj->Contains(constant::Name::Producer))
+	if (!_obj->Contains(constant::Name::Producer)) {
 		return false;
+	}
 
 	result = _obj->FindAs<syntax::StringObjectPtr>(constant::Name::Producer);
 	return true;
 }
 
 bool DocumentInfo::CreationDate(OutputDatePtr& result) const {
-	if (!_obj->Contains(constant::Name::CreationDate))
+	if (!_obj->Contains(constant::Name::CreationDate)) {
 		return false;
+	}
 
 	result = _obj->FindAs<syntax::StringObjectPtr>(constant::Name::CreationDate);
 	return true;
@@ -73,11 +80,12 @@ bool DocumentInfo::ModificationDate(OutputDatePtr& result) const {
 }
 
 bool DocumentInfo::Trapped(DocumentTrapped& result) const {
-	if (!_obj->Contains(constant::Name::Trapped))
+	if (!_obj->Contains(constant::Name::Trapped)) {
 		return false;
+	}
 
-	auto trapped = _obj->Find(constant::Name::Trapped);
 	BufferPtr value;
+	auto trapped = _obj->Find(constant::Name::Trapped);
 	if (syntax::ObjectUtils::IsType<syntax::NameObjectPtr>(trapped)) {
 		auto name = syntax::ObjectUtils::ConvertTo<syntax::NameObjectPtr>(trapped);
 		value = name->GetValue();
