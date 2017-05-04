9f34deb7d09e44211021f26ef197b7af4e97796b
Juraj Zikmund <jur.zikmund@gmail.com>
2017-05-04 21:09:40 +0200

    Solved library dependencies using LoadLibraryEx

--- src/gotchangpdf.net/LibraryInstance.cs
+++ src/gotchangpdf.net/LibraryInstance.cs
@@ -49,20 +49,8 @@ namespace gotchangpdf.net
                 libraryPath = Path.GetFullPath(X64_LIBRARY_PATH);
             }
 
-            // Determine if the libary path is included in environment PATH variable
-            IDictionary environmentVars = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
-            string pathVariable = environmentVars[PATH_ENVIRONMENT_VARIABLE] as string;
-            string[] pathVariables = pathVariable.Split(';');
-
-            // Append path, if not included
-            string libraryDirectoryName = Path.GetDirectoryName(libraryPath);
-            if (!pathVariables.Contains(libraryDirectoryName)) {
-                string newPathVariable = $"{pathVariable};{libraryDirectoryName}";
-                Environment.SetEnvironmentVariable(PATH_ENVIRONMENT_VARIABLE, newPathVariable);
-            }
-
             // Call the load library native function
-            m_handle = LoadLibrary(libraryPath);
+            m_handle = LoadLibraryEx(libraryPath, IntPtr.Zero, LoadLibraryFlags.LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR | LoadLibraryFlags.LOAD_LIBRARY_SEARCH_SYSTEM32);
 
             // Could not library or it's dependencies
             if (m_handle == IntPtr.Zero) {
@@ -107,8 +95,8 @@ namespace gotchangpdf.net
             return Marshal.ReadInt32(constantAddress);
         }
 
-        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
-        static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)]string lpFileName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
@@ -116,5 +104,21 @@ namespace gotchangpdf.net
 
         [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
         static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [Flags]
+        enum LoadLibraryFlags : uint
+        {
+            DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+            LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
+            LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
+            LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
+            LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
+            LOAD_LIBRARY_SEARCH_APPLICATION_DIR = 0x00000200,
+            LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000,
+            LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100,
+            LOAD_LIBRARY_SEARCH_SYSTEM32 = 0x00000800,
+            LOAD_LIBRARY_SEARCH_USER_DIRS = 0x00000400,
+            LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008
+        }
     }
 }
