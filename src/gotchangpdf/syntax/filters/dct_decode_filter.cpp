58586b6b0e663132e435b46aac3d07a9e5a656aa
Juraj Zikmund <jur.zikmund@gmail.com>
2017-08-05 10:24:14 +0200

    JPEG decompression refactor

--- src/gotchangpdf/syntax/filters/dct_decode_filter.cpp
+++ src/gotchangpdf/syntax/filters/dct_decode_filter.cpp
@@ -12,20 +12,18 @@ namespace syntax {
 
 #if defined(GOTCHANG_PDF_HAVE_JPEG)
 
-void JPegErrorExit(j_common_ptr ptr) {
-	ptr;
+void error_exit(j_common_ptr ptr) {
 	char buffer[JMSG_LENGTH_MAX];
-
-	/* Create the message */
 	(*ptr->err->format_message) (ptr, buffer);
 
-	jpeg_destroy(ptr);
+	LOG_ERROR_GLOBAL << "JPEG decompression exited with error: " << buffer;
 }
 
-void JPegErrorOutput(j_common_ptr ptr, int val) {
-	ptr; val;
+void output_message(j_common_ptr ptr) {
 	char buffer[JMSG_LENGTH_MAX];
 	(*ptr->err->format_message) (ptr, buffer);
+
+	LOG_WARNING_GLOBAL << "JPEG decompression reported message: " << buffer;
 }
 
 void init_source(j_decompress_ptr ptr) {
@@ -34,11 +32,18 @@ void init_source(j_decompress_ptr ptr) {
 
 boolean fill_input_buffer(j_decompress_ptr ptr) {
 	ptr;
+
+	assert(!"This method is not handled");
+	LOG_ERROR_GLOBAL << "JPEG decompression failure. Please report this issue";
+
 	return TRUE;
 }
 
 void skip_input_data(j_decompress_ptr ptr, long num_bytes) {
 	ptr; num_bytes;
+
+	assert(!"This method is not handled");
+	LOG_ERROR_GLOBAL << "JPEG decompression failure. Please report this issue";
 }
 
 void term_source(j_decompress_ptr ptr) {
@@ -55,16 +60,22 @@ BufferPtr DCTDecodeFilter::Decode(std::istream& src, types::stream_size length, 
 
 #if defined(GOTCHANG_PDF_HAVE_JPEG)
 
-	BufferPtr result;
-
-	jpeg_decompress_struct jpeg;
-	jpeg_error_mgr err;
+	jpeg_decompress_struct jpeg = {0};
+	jpeg_error_mgr err = {0};
 
 	jpeg.err = jpeg_std_error(&err);
-	err.error_exit = &JPegErrorExit;
-	err.emit_message = &JPegErrorOutput;
+	err.error_exit = &error_exit;
+	err.output_message = &output_message;
 
 	jpeg_create_decompress(&jpeg);
+
+	// We want to capture input by value, because it might be out of scope
+	// In order to call non-const method we have to tag the lambda mutable
+	auto cleanup_lambda = [jpeg]() mutable {
+		jpeg_destroy_decompress(&jpeg);
+	};
+
+	SCOPE_GUARD(cleanup_lambda);
 
 	jpeg.src = static_cast<jpeg_source_mgr*>(
 		(jpeg.mem->alloc_small)
@@ -77,55 +88,66 @@ BufferPtr DCTDecodeFilter::Decode(std::istream& src, types::stream_size length, 
 	jpeg.src->resync_to_restart = jpeg_resync_to_restart; /* use default method */
 	jpeg.src->term_source = term_source;
 
-	BufferPtr input = make_deferred<Buffer>(static_cast<size_t>(length));
-	src.read(input->data(), length);
+	size_t length_converted = ValueConvertUtils::SafeConvert<size_t>(length);
+
+	BufferPtr input = make_deferred<Buffer>(length_converted);
+	src.read(input->data(), length_converted);
 
 	jpeg.src->next_input_byte = reinterpret_cast<unsigned char*>(input->data());
-	jpeg.src->bytes_in_buffer = static_cast<size_t>(length);
+	jpeg.src->bytes_in_buffer = length_converted;
 
-	jpeg_read_header(&jpeg, TRUE);
-	jpeg_start_decompress(&jpeg);
-
-	BufferPtr  buffer;
-	JSAMPARRAY pBuffer;
-	long       lRowBytes = jpeg.output_width * jpeg.output_components;
-	const int  iComponents = jpeg.output_components;
-
-	// pBuffer will be deleted by jpeg_destroy_decompress
-	pBuffer = (*jpeg.mem->alloc_sarray)(reinterpret_cast<j_common_ptr>(&jpeg), JPOOL_IMAGE, 1500, 1);
-	buffer->resize(lRowBytes);
-
-	while (jpeg.output_scanline < jpeg.output_height) {
-		int lines = jpeg_read_scanlines(&jpeg, pBuffer, 1);
-		assert(1 == lines); lines;
-		if (iComponents == 4) {
-			for (unsigned int i = 0, c = 0; i < jpeg.output_width; i++, c += 4) {
-				buffer[c] = pBuffer[0][i * 4];
-				buffer[c + 1] = pBuffer[0][i * 4 + 1];
-				buffer[c + 2] = pBuffer[0][i * 4 + 2];
-				buffer[c + 3] = pBuffer[0][i * 4 + 3];
-			}
-		}
-
-		if (iComponents == 3) {
-			for (unsigned int i = 0, c = 0; i < jpeg.output_width; i++, c += 3) {
-				buffer[c] = pBuffer[0][i * 3];
-				buffer[c + 1] = pBuffer[0][i * 3 + 1];
-				buffer[c + 2] = pBuffer[0][i * 3 + 2];
-			}
-		}
-
-		if (iComponents == 1) {
-			std::memcpy(buffer->data(), pBuffer[0], jpeg.output_width);
-		} else {
-		}
-
-		result->insert(result->end(), buffer->begin(), buffer->end());
+	int header = jpeg_read_header(&jpeg, TRUE);
+	if (header != JPEG_HEADER_OK) {
+		throw GeneralException("Could not read jpeg header");
 	}
 
+	boolean started = jpeg_start_decompress(&jpeg);
+	if (started != TRUE) {
+		throw GeneralException("Could not start jpeg decompression");
+	}
 
-	jpeg_finish_decompress(&jpeg);
-	jpeg_destroy_decompress(&jpeg);
+	size_t row_bytes = SafeMultiply<size_t, JDIMENSION>(jpeg.output_width, jpeg.output_components);
+	JSAMPARRAY jpeg_buffer = (*jpeg.mem->alloc_sarray)(reinterpret_cast<j_common_ptr>(&jpeg), JPOOL_IMAGE, row_bytes, 1);
+
+	BufferPtr result;
+	Buffer buffer(row_bytes);
+
+	while (jpeg.output_scanline < jpeg.output_height) {
+		JDIMENSION lines = jpeg_read_scanlines(&jpeg, jpeg_buffer, 1);
+		assert(1 == lines); lines;
+
+		if (jpeg.output_components == 4) {
+			for (uint32_t i = 0; i < jpeg.output_width; i++) {
+				uint32_t offset = SafeMultiply<uint32_t, uint32_t>(i, 4);
+				assert(offset < buffer.size());
+
+				buffer[offset] = jpeg_buffer[0][offset];
+				buffer[offset + 1] = jpeg_buffer[0][offset + 1];
+				buffer[offset + 2] = jpeg_buffer[0][offset + 2];
+				buffer[offset + 3] = jpeg_buffer[0][offset + 3];
+			}
+		} else if (jpeg.output_components == 3) {
+			for (uint32_t i = 0; i < jpeg.output_width; i++) {
+				uint32_t offset = SafeMultiply<uint32_t, uint32_t>(i, 3);
+				assert(offset < buffer.size());
+				buffer[offset] = jpeg_buffer[0][offset];
+				buffer[offset + 1] = jpeg_buffer[0][offset + 1];
+				buffer[offset + 2] = jpeg_buffer[0][offset + 2];
+			}
+		} else if (jpeg.output_components == 1) {
+			std::memcpy(buffer.data(), jpeg_buffer[0], jpeg.output_width);
+		} else {
+			assert(!"Unknown JPEG components");
+			LOG_ERROR_GLOBAL << "Unknown JPEG components";
+		}
+
+		result->insert(result->end(), buffer.begin(), buffer.end());
+	}
+
+	boolean finished = jpeg_finish_decompress(&jpeg);
+	if (finished != TRUE) {
+		throw GeneralException("Could not finish jpeg decompression");
+	}
 
 	return result;
 
