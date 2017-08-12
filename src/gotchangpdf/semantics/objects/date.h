0354e8b541b24b42d267b240ef2e5ac93dee6f8b
Juraj Zikmund <jur.zikmund@gmail.com>
2017-08-12 20:13:01 +0200

    Time utilities now use 32-bit integer

--- src/gotchangpdf/semantics/objects/date.h
+++ src/gotchangpdf/semantics/objects/date.h
@@ -13,28 +13,28 @@ class Date : public HighLevelObject<syntax::StringObjectPtr> {
 public:
 	explicit Date(syntax::StringObjectPtr root);
 
-	int GetYear(void) const noexcept { return m_year; }
-	int GetMonth(void) const noexcept { return m_month; }
-	int GetDay(void) const noexcept { return m_day; }
-	int GetHour(void) const noexcept { return m_hour; }
-	int GetMinute(void) const noexcept { return m_minute; }
-	int GetSecond(void) const noexcept { return m_second; }
+	int32_t GetYear(void) const noexcept { return m_year; }
+	int32_t GetMonth(void) const noexcept { return m_month; }
+	int32_t GetDay(void) const noexcept { return m_day; }
+	int32_t GetHour(void) const noexcept { return m_hour; }
+	int32_t GetMinute(void) const noexcept { return m_minute; }
+	int32_t GetSecond(void) const noexcept { return m_second; }
 	Timezone GetTimezone(void) const noexcept { return m_timezone; }
-	int GetHourOffset(void) const noexcept { return m_hour_offset; }
-	int GetMinuteOffset(void) const noexcept { return m_minute_offset; }
+	int32_t GetHourOffset(void) const noexcept { return m_hour_offset; }
+	int32_t GetMinuteOffset(void) const noexcept { return m_minute_offset; }
 
 	static DatePtr GetCurrentDate();
 
 private:
-	int m_year = 1900;
-	int m_month = 1;
-	int m_day = 1;
-	int m_hour = 0;
-	int m_minute = 0;
-	int m_second = 0;
+	int32_t m_year = 1900;
+	int32_t m_month = 1;
+	int32_t m_day = 1;
+	int32_t m_hour = 0;
+	int32_t m_minute = 0;
+	int32_t m_second = 0;
 	Timezone m_timezone = Timezone::UTC;
-	int m_hour_offset = 0;
-	int m_minute_offset = 0;
+	int32_t m_hour_offset = 0;
+	int32_t m_minute_offset = 0;
 };
 
 } // semantics
--- src/gotchangpdf/utils/time_utils.h
+++ src/gotchangpdf/utils/time_utils.h
@@ -15,36 +15,36 @@ enum class Timezone : char {
 
 class TimeInfo {
 public:
-	int GetYear(void) const noexcept { return m_year; }
-	int GetMonth(void) const noexcept { return m_month; }
-	int GetDay(void) const noexcept { return m_day; }
-	int GetHour(void) const noexcept { return m_hour; }
-	int GetMinute(void) const noexcept { return m_minute; }
-	int GetSecond(void) const noexcept { return m_second; }
+	int32_t GetYear(void) const noexcept { return m_year; }
+	int32_t GetMonth(void) const noexcept { return m_month; }
+	int32_t GetDay(void) const noexcept { return m_day; }
+	int32_t GetHour(void) const noexcept { return m_hour; }
+	int32_t GetMinute(void) const noexcept { return m_minute; }
+	int32_t GetSecond(void) const noexcept { return m_second; }
 	Timezone GetTimezone(void) const noexcept { return m_timezone; }
-	int GetHourOffset(void) const noexcept { return m_hour_offset; }
-	int GetMinuteOffset(void) const noexcept { return m_minute_offset; }
+	int32_t GetHourOffset(void) const noexcept { return m_hour_offset; }
+	int32_t GetMinuteOffset(void) const noexcept { return m_minute_offset; }
 
-	void SetYear(int value) noexcept { m_year = value; }
-	void SetMonth(int value) noexcept { m_month = value; }
-	void SetDay(int value) noexcept { m_day = value; }
-	void SetHour(int value) noexcept { m_hour = value; }
-	void SetMinute(int value) noexcept { m_minute = value; }
-	void SetSecond(int value) noexcept { m_second = value; }
+	void SetYear(int32_t value) noexcept { m_year = value; }
+	void SetMonth(int32_t value) noexcept { m_month = value; }
+	void SetDay(int32_t value) noexcept { m_day = value; }
+	void SetHour(int32_t value) noexcept { m_hour = value; }
+	void SetMinute(int32_t value) noexcept { m_minute = value; }
+	void SetSecond(int32_t value) noexcept { m_second = value; }
 	void SetTimezone(Timezone value) noexcept { m_timezone = value; }
-	void SetHourOffset(int value) noexcept { m_hour_offset = value; }
-	void SetMinuteOffset(int value) noexcept { m_minute_offset = value; }
+	void SetHourOffset(int32_t value) noexcept { m_hour_offset = value; }
+	void SetMinuteOffset(int32_t value) noexcept { m_minute_offset = value; }
 
 private:
-	int m_year = 1900;
-	int m_month = 1;
-	int m_day = 1;
-	int m_hour = 0;
-	int m_minute = 0;
-	int m_second = 0;
+	int32_t m_year = 1900;
+	int32_t m_month = 1;
+	int32_t m_day = 1;
+	int32_t m_hour = 0;
+	int32_t m_minute = 0;
+	int32_t m_second = 0;
 	Timezone m_timezone = Timezone::UTC;
-	int m_hour_offset = 0;
-	int m_minute_offset = 0;
+	int32_t m_hour_offset = 0;
+	int32_t m_minute_offset = 0;
 };
 
 class TimeUtils {
