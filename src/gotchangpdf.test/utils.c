f1a3be6184843558872bedb8b3d7aa5009332e6e
Juraj Zikmund <jur.zikmund@gmail.com>
2017-08-24 17:48:23 +0200

    printf for 64-bit fix

--- src/gotchangpdf.test/utils.c
+++ src/gotchangpdf.test/utils.c
@@ -17,8 +17,8 @@ error_type process_buffer(BufferHandle buffer, int nested) {
 	string_type data = NULL;
 	char* local_string = NULL;
 	size_type size = 0;
-	size_type size_converted = 0;
 	size_type print_size = 0;
+	unsigned long long size_converted = 0;
 
 	print_spaces(nested);
 	printf("Buffer begin\n");
@@ -26,23 +26,25 @@ error_type process_buffer(BufferHandle buffer, int nested) {
 	RETURN_ERROR_IF_NOT_SUCCESS(Buffer_GetData(buffer, &data, &size));
 
 	if (size >= SIZE_MAX) {
-		printf("Buffer size is too big: %ld bytes\n", size);
+		unsigned long long size_long_long = size;
+		printf("Buffer size is too big: %lld bytes\n", size_long_long);
 		return GOTCHANG_PDF_TEST_ERROR_FAILURE;
 	}
 
-	size_converted = (size_type) size;
-	print_size = size_converted > 20 ? 10 : size_converted;
+	print_size = size > 20 ? 10 : size;
+	size_converted = size;
 
 	local_string = (char*) calloc(sizeof(char), print_size + 1);
 	if (NULL == local_string) {
-		printf("Could not allocate memory: %ld bytes\n", print_size + 1);
+		unsigned long long print_size_converted = print_size;
+		printf("Could not allocate memory: %lld bytes\n", print_size_converted + 1);
 		return GOTCHANG_PDF_TEST_ERROR_FAILURE;
 	}
 
 	memcpy(local_string, data, print_size);
 
 	print_spaces(nested + 1);
-	printf("Size: %ld\n", size_converted);
+	printf("Size: %lld\n", size_converted);
 	print_spaces(nested + 1);
 	printf("Data: %s\n", local_string);
 
@@ -117,13 +119,15 @@ error_type print_last_error() {
 	RETURN_ERROR_IF_NOT_SUCCESS(Errors_GetLastErrorMessageLength(&length));
 
 	if (length >= SIZE_MAX) {
-		printf("Buffer size is too big: %ld bytes\n", length);
+		unsigned long long length_converted = length;
+		printf("Buffer size is too big: %lld bytes\n", length_converted);
 		return GOTCHANG_PDF_TEST_ERROR_FAILURE;
 	}
 
 	message = (char*) calloc(sizeof(char), length + 1);
 	if (NULL == message) {
-		printf("Could not allocate memory: %ld bytes\n", length + 1);
+		unsigned long long length_converted = length;
+		printf("Could not allocate memory: %lld bytes\n", length_converted + 1);
 		return GOTCHANG_PDF_TEST_ERROR_FAILURE;
 	}
 
