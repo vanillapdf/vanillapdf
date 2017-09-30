ca211b41caaebd2ce1becb6b1b27076f91acaf25
Juraj Zikmund <jur.zikmund@gmail.com>
2017-10-01 00:40:16 +0200

    Library info parameter validation

--- src/gotchangpdf/implementation/c_library_info.cpp
+++ src/gotchangpdf/implementation/c_library_info.cpp
@@ -8,39 +8,53 @@ using namespace gotchangpdf;
 
 GOTCHANG_PDF_API error_type CALLING_CONVENTION LibraryInfo_GetVersionMajor(integer_type* result)
 {
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+
 	*result = LibraryInfo::MajorVersion();
 	return GOTCHANG_PDF_ERROR_SUCCESS;
 }
 
 GOTCHANG_PDF_API error_type CALLING_CONVENTION LibraryInfo_GetVersionMinor(integer_type* result)
 {
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+
 	*result = LibraryInfo::MinorVersion();
 	return GOTCHANG_PDF_ERROR_SUCCESS;
 }
 
 GOTCHANG_PDF_API error_type CALLING_CONVENTION LibraryInfo_GetVersionPatch(integer_type* result)
 {
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+
 	*result = LibraryInfo::PatchVersion();
 	return GOTCHANG_PDF_ERROR_SUCCESS;
 }
 
 GOTCHANG_PDF_API error_type CALLING_CONVENTION LibraryInfo_GetAuthor(string_type* result)
 {
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+
 	*result = LibraryInfo::Author();
 	return GOTCHANG_PDF_ERROR_SUCCESS;
 }
 
 GOTCHANG_PDF_API error_type CALLING_CONVENTION LibraryInfo_GetBuildDay(integer_type* result) {
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+
 	*result = LibraryInfo::BuildDay();
 	return GOTCHANG_PDF_ERROR_SUCCESS;
 }
 
 GOTCHANG_PDF_API error_type CALLING_CONVENTION LibraryInfo_GetBuildMonth(integer_type* result) {
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+
 	*result = LibraryInfo::BuildMonth();
 	return GOTCHANG_PDF_ERROR_SUCCESS;
 }
 
 GOTCHANG_PDF_API error_type CALLING_CONVENTION LibraryInfo_GetBuildYear(integer_type* result) {
+	RETURN_ERROR_PARAM_VALUE_IF_NULL(result);
+
 	*result = LibraryInfo::BuildYear();
 	return GOTCHANG_PDF_ERROR_SUCCESS;
 }
