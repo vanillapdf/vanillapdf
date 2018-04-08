6e9c4dce19e72679687805927c03c86b910532b5
Juraj Zikmund <jur.zikmund@gmail.com>
2018-04-08 17:48:03 +0200

    Custom navbar is only on main page

--- web/build/main/assets/js/script.js
+++ web/build/main/assets/js/script.js
@@ -99,21 +99,6 @@ $(function() {
   |
   */
   
-  // Custom navigation bar, that is light on the cover page and dark on the others
-
-  $(window).on('scroll', function() {
-    var window_top = $(window).scrollTop();
-    var navbar_height = page.navbar.innerHeight();
-
-    if (window_top > navbar_height) {
-      page.navbar.addClass('navbar-dark');
-      page.navbar.removeClass('navbar-light');
-    }
-    else {
-      page.navbar.addClass('navbar-light');
-      page.navbar.removeClass('navbar-dark');
-    }
-  });
 
 });
 
