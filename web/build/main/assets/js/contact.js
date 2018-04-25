a6bf0fb7860a8299d1c06142d15b6daf68dec91f
Juraj Zikmund <jur.zikmund@gmail.com>
2018-04-25 23:38:47 +0200

    Contact page js fading fix for temporary license

--- web/build/main/assets/js/contact.js
+++ web/build/main/assets/js/contact.js
@@ -12,28 +12,40 @@ function EnableReport() {
 	document.getElementById("btn-report").disabled = false;
 }
 
+function EnableLicense() {
+	document.getElementById("btn-license").disabled = false;
+}
+
 /* Button actions */
 
 function GeneralQuestionClick() {
-	$("#section-report").fadeOut(function() {
-		$("#section-discount").fadeOut(function() {
-			$("#section-contact").fadeIn();
-		});
+	$("#section-license").fadeOut();
+	$("#section-report").fadeOut();
+	$("#section-discount").fadeOut(function() {
+		$("#section-contact").fadeIn();
+	});
+}
+
+function TemporaryLicenseClick() {
+	$("#section-contact").fadeOut();
+	$("#section-report").fadeOut();
+	$("#section-discount").fadeOut(function() {
+		$("#section-license").fadeIn();
 	});
 }
 
 function ReportBugClick() {
-	$("#section-contact").fadeOut(function() {
-		$("#section-discount").fadeOut(function() {
-			$("#section-report").fadeIn();
-		});
+	$("#section-contact").fadeOut();
+	$("#section-license").fadeOut();
+	$("#section-discount").fadeOut(function() {
+		$("#section-report").fadeIn();
 	});
 }
 
 function DiscountClick() {
+	$("#section-contact").fadeOut();
+	$("#section-license").fadeOut();
 	$("#section-report").fadeOut(function() {
-		$("#section-contact").fadeOut(function() {
-			$("#section-discount").fadeIn();
-		});
+		$("#section-discount").fadeIn();
 	});
 }
