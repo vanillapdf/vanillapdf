57546a4876a81b54a96d788621f0c0b06e44db58
Juraj Zikmund <jur.zikmund@gmail.com>
2018-06-29 20:17:33 +0200

    .NET wrapper printable error text

--- src/vanillapdf.net/Errors.cs
+++ src/vanillapdf.net/Errors.cs
@@ -16,6 +16,24 @@ namespace vanillapdf.net
             return code;
         }
 
+        public static string GetPrintableErrorText(UInt32 error)
+        {
+            UInt32 lengthResult = NativeMethods.Errors_GetPrintableErrorTextLength(error, out UInt32 length);
+            if (lengthResult != ReturnValues.ERROR_SUCCESS) {
+                throw new Exception("Could not get last error message length");
+            }
+
+            int convertedLength = Convert.ToInt32(length);
+            StringBuilder sb = new StringBuilder(convertedLength);
+
+            UInt32 messageResult = NativeMethods.Errors_GetPrintableErrorText(error, sb, length);
+            if (messageResult != ReturnValues.ERROR_SUCCESS) {
+                throw new Exception("Could not get last error message");
+            }
+
+            return sb.ToString();
+        }
+
         public static string GetLastErrorMessage()
         {
             UInt32 lengthResult = NativeMethods.Errors_GetLastErrorMessageLength(out UInt32 length);
@@ -45,11 +63,19 @@ namespace vanillapdf.net
         private static class NativeMethods
         {
             public static ErrorsGetLastErrorDelgate Errors_GetLastError = LibraryInstance.GetFunction<ErrorsGetLastErrorDelgate>("Errors_GetLastError");
+            public static ErrorsGetPrintableErrorTextDelegate Errors_GetPrintableErrorText = LibraryInstance.GetFunction<ErrorsGetPrintableErrorTextDelegate>("Errors_GetPrintableErrorText");
+            public static ErrorsGetPrintableErrorTextLengthDelegate Errors_GetPrintableErrorTextLength = LibraryInstance.GetFunction<ErrorsGetPrintableErrorTextLengthDelegate>("Errors_GetPrintableErrorTextLength");
             public static ErrorsGetLastErrorMessageDelgate Errors_GetLastErrorMessage = LibraryInstance.GetFunction<ErrorsGetLastErrorMessageDelgate>("Errors_GetLastErrorMessage");
             public static ErrorsGetLastErrorMessageLengthDelgate Errors_GetLastErrorMessageLength = LibraryInstance.GetFunction<ErrorsGetLastErrorMessageLengthDelgate>("Errors_GetLastErrorMessageLength");
 
             [UnmanagedFunctionPointer(Utils.LibraryCallingConvention)]
             public delegate UInt32 ErrorsGetLastErrorDelgate(out UInt32 result);
+
+            [UnmanagedFunctionPointer(Utils.LibraryCallingConvention)]
+            public delegate UInt32 ErrorsGetPrintableErrorTextDelegate(UInt32 error, StringBuilder sb, UInt32 size);
+
+            [UnmanagedFunctionPointer(Utils.LibraryCallingConvention)]
+            public delegate UInt32 ErrorsGetPrintableErrorTextLengthDelegate(UInt32 error, out UInt32 result);
 
             [UnmanagedFunctionPointer(Utils.LibraryCallingConvention)]
             public delegate UInt32 ErrorsGetLastErrorMessageDelgate(StringBuilder sb, UInt32 size);
