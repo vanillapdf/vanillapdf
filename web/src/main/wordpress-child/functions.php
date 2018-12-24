ed455e5ac1e1a72de4a8c653040015ff83db023c
Juraj Zikmund <jur.zikmund@gmail.com>
2018-12-24 22:56:28 +0100

    Wordpress child theme

---
+++ web/src/main/wordpress-child/functions.php
@@ -0,0 +1,17 @@
+ <?php
+/**
+ * Vanilla.PDF functions and definitions
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @link https://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * {@link https://codex.wordpress.org/Plugin_API}
+ *
+ * @package WordPress
+ * @subpackage Vanilla.PDF Child
+ * @since Vanilla.PDF 1.0
+ */
