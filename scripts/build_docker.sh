e39d2021bb211cf047a0d91b9bf84c7c7f68323c
Juraj Zikmund <jur.zikmund@gmail.com>
2018-12-27 16:32:59 +0100

    Dcoker images, 32-bit compat on 64-bit machine

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -3,5 +3,8 @@
 docker build -t registry.gitlab.com/jurzik/vanillapdf/vanillapdf-centos:latest -f centos_dockerfile .
 docker push registry.gitlab.com/jurzik/vanillapdf/vanillapdf-centos:latest
 
+docker build -t registry.gitlab.com/jurzik/vanillapdf/vanillapdf-ubuntu:latest -f ubuntu_dockerfile .
+docker push registry.gitlab.com/jurzik/vanillapdf/vanillapdf-ubuntu:latest
+
 # Clean dangling images after the build
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
