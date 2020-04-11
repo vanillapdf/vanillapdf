e5dfcee3d718f328d82e1ae755c96d8410595a2e
Juraj Zikmund <jur.zikmund@gmail.com>
2020-04-11 21:36:33 +0200

    Split projects into separate repos

--- web/build/wordpress/assets/php/sendmail.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require 'sendmail_inc.php';
-echo SendMail();
-
-?>
--- web/src/main/wordpress/inc/notes/1.3.0.php
+++
@@ -1,9 +0,0 @@
-<div>
-<h4>Release notes:</h4>
-<ul>
-	<li>IO streams exposed on API - <a id="io-streams" href="<?php echo get_versions_folder() . '/' . LATEST_VERSION; ?>/doc/hierarchy.html">details</a>
-	<li>Added tools project to the release package
-	<li>Fixed WiX upgrade from older versions
-	<li>Extended examples with customized write callbacks - <a id="extended-examples" href="<?php echo get_versions_folder() . '/' . LATEST_VERSION; ?>/doc/page_examples.html">details</a>
-</ul>
-</div>
