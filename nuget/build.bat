7a23d623789bd72b796e620512f35a697e1dc665
Juraj Zikmund <jur.zikmund@gmail.com>
2020-11-01 09:20:38 +0100

    Nuget now working with c++

--- nuget/build.bat
+++ nuget/build.bat
@@ -1,12 +1,11 @@
-SET var=%cd%
-set msbuild="C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe"
-
 :: ------------- win-x86_v140 -------------
 mkdir "build\win-x86_v140"
 cd "build\win-x86_v140"
 
 call cmake -G "Visual Studio 14 2015" -DCMAKE_GENERATOR_PLATFORM=Win32 -DCMAKE_GENERATOR_TOOLSET=v140 ..\..\..
-call %msbuild% vanillapdf.sln /t:vanillapdf /p:Configuration=Release
+call cmake --build . --config Release --target ALL_BUILD
+call cmake --build . --config Release --target RUN_TESTS
+call cmake --build . --config Release --target PACKAGE
 
 cd ..\..
 :: ------------- win-x86_v140 -------------
@@ -16,7 +15,9 @@ mkdir "build\win-x64_v140"
 cd "build\win-x64_v140"
 
 call cmake -G "Visual Studio 14 2015" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_GENERATOR_TOOLSET=v140 ..\..\..
-call %msbuild% vanillapdf.sln /t:vanillapdf /p:Configuration=Release
+call cmake --build . --config Release --target ALL_BUILD
+call cmake --build . --config Release --target RUN_TESTS
+call cmake --build . --config Release --target PACKAGE
 
 cd ..\..
 :: ------------- win-x64_v140 -------------
