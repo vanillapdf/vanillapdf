8cb144eef201fcd7f7a4def549b3228dd3672566
Juraj Zikmund <jur.zikmund@gmail.com>
2022-08-17 21:11:39 +0200

    Switch to static library link for windows

--- nuget/build.bat
+++ nuget/build.bat
@@ -1,26 +1,26 @@
-:: ------------- win-x86_v140 -------------
-mkdir "build\win-x86_v140"
-cd "build\win-x86_v140"
+:: ------------- win-x86_v143 -------------
+mkdir "build\win-x86_v143"
+cd "build\win-x86_v143"
 
-call cmake -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=Win32 -DCMAKE_GENERATOR_TOOLSET=v140 ..\..\..
+call cmake -G "Visual Studio 17 2022" -DCMAKE_GENERATOR_PLATFORM=Win32 -DCMAKE_GENERATOR_TOOLSET=v143 ..\..\..
 call cmake --build . --config Release --target ALL_BUILD
 call cmake --build . --config Release --target RUN_TESTS
 call cmake --build . --config Release --target PACKAGE
 
 cd ..\..
-:: ------------- win-x86_v140 -------------
+:: ------------- win-x86_v143 -------------
 
-:: ------------- win-x64_v140 -------------
-mkdir "build\win-x64_v140"
-cd "build\win-x64_v140"
+:: ------------- win-x64_v143 -------------
+mkdir "build\win-x64_v143"
+cd "build\win-x64_v143"
 
-call cmake -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_GENERATOR_TOOLSET=v140 ..\..\..
+call cmake -G "Visual Studio 17 2022" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_GENERATOR_TOOLSET=v143 ..\..\..
 call cmake --build . --config Release --target ALL_BUILD
 call cmake --build . --config Release --target RUN_TESTS
 call cmake --build . --config Release --target PACKAGE
 
 cd ..\..
-:: ------------- win-x64_v140 -------------
+:: ------------- win-x64_v143 -------------
 
 :: ------------- NUGET_FINAL -------------
 ::rm vanillapdf.nupkg
