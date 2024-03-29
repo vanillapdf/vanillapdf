11d9b09e786d287afcd2be11475e716799928a8c
Juraj Zikmund <jur.zikmund@gmail.com>
2024-03-29 19:26:31 +0100

    Log on overwrite
    In case the dictionary entry is being overwritten add log info

--- src/vanillapdf/syntax/objects/dictionary_object.cpp
+++ src/vanillapdf/syntax/objects/dictionary_object.cpp
@@ -192,6 +192,13 @@ void DictionaryObject::Insert(NameObjectPtr name, ContainableObjectPtr value, bo
 	auto result = _list.insert(pair);
 
 	// The pair::second element in the pair is set to true if a new element was inserted or false if an equivalent key already existed.
+	if (!result.second) {
+		auto weak_file = GetFile();
+		auto file = weak_file.GetReference();
+		auto log_scope = file->GetFilenameString();
+
+		LOG_INFO(log_scope) << "Overwriting dictionary entry for key: " << name->ToString();
+	}
 
 	name->SetOwner(Object::GetWeakReference());
 	value->SetOwner(Object::GetWeakReference());
