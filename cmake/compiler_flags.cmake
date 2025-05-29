16ffd01064fdf088918e4de8da23aab4ce4fe788
Juraj Zikmund <jur.zikmund@gmail.com>
2025-05-30 01:39:29 +0200

    NEW: Support static CRT
    By default we link dynamic CRT on all platforms.
    With VANILLAPDF_USE_STATIC_CRT flag we can enable static linking.
    We are using static linking for nuget packages to produce single file without any dependencies.

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -54,35 +54,3 @@ if(CMAKE_COMPILER_IS_GNUCXX)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-parameter")
 
 endif(CMAKE_COMPILER_IS_GNUCXX)
-
-if(MSVC)
-
-    # Enable maximum warning level and treat warnings as errors
-    add_compile_options(/W4)
-
-    # We want to statically link most of the things on Windows
-    # as it is hell to deploy multiple binaries alongside
-
-    # UPDATE 2022:
-    # Some dependencies such as libjpeg and OpenSSL do not support static CRT
-    # This means that we have to ship CRT libraries in any case
-    # Let's use this and reduce the libvanillapdf size by dynamically linking the CRT
-
-    # Update 2025:
-    # Switching to CMAKE_MSVC_RUNTIME_LIBRARY
-    # set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} /MTd")
-    # set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
-    # 
-    # set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} /MT")
-    # set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
-    # 
-    # set(CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS_MINSIZEREL} /MT")
-    # set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_MINSIZEREL} /MT")
-    # 
-    # set(CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO} /MT")
-    # set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /MT")
-
-    # Use static runtime: /MT or /MTd
-    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" CACHE STRING "" FORCE)
-
-endif(MSVC)
