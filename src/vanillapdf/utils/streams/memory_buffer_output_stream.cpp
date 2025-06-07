9d70d7818f5b3d2966f5ba5be9664014fc467424
Juraj Zikmund <jur.zikmund@gmail.com>
2025-06-07 14:12:24 +0200

    FIX: Conversion issue

--- src/vanillapdf/utils/streams/memory_buffer_output_stream.cpp
+++ src/vanillapdf/utils/streams/memory_buffer_output_stream.cpp
@@ -12,8 +12,10 @@ void MemoryBufferOutputStream::Write(const Buffer& data) {
 }
 
 void MemoryBufferOutputStream::Write(const Buffer& data, types::stream_size size) {
+    auto size_converted = ValueConvertUtils::SafeConvert<size_t>(size);
+
     auto str = data.ToStringView();
-    auto substring = str.substr(0, size);
+    auto substring = str.substr(0, size_converted);
 
     m_buffer.append(substring);
     m_position += size;
