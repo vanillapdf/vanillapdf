e87c4d3e85b45cc5b7bca3af901925132b328531
Juraj Zikmund <jur.zikmund@gmail.com>
2025-07-05 19:55:36 +0200

    Switch core NuGet packaging job to Ubuntu (#82)

---
+++ cmake/nuget.cmake
@@ -0,0 +1,22 @@
+cmake_minimum_required(VERSION 3.15)
+
+# Configure NuGet project templates. This script can be included from a regular
+# build or executed directly via `cmake -P`. In both cases
+# `CMAKE_CURRENT_SOURCE_DIR` resolves to the project root.
+
+include(${CMAKE_CURRENT_LIST_DIR}/version.cmake)
+configure_file(
+    ${CMAKE_CURRENT_SOURCE_DIR}/nuget/vanillapdf.runtime.csproj.in
+    ${CMAKE_CURRENT_SOURCE_DIR}/nuget/vanillapdf.runtime.csproj
+    @ONLY
+)
+configure_file(
+    ${CMAKE_CURRENT_SOURCE_DIR}/nuget/vanillapdf_net.targets.in
+    ${CMAKE_CURRENT_SOURCE_DIR}/nuget/vanillapdf_net.targets
+    @ONLY
+)
+configure_file(
+    ${CMAKE_CURRENT_SOURCE_DIR}/nuget/vanillapdf.csproj.in
+    ${CMAKE_CURRENT_SOURCE_DIR}/nuget/vanillapdf.csproj
+    @ONLY
+)
