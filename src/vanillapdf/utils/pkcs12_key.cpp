3b91862aa2df1b88db4affd4cdc460322c96e59a
Juraj Zikmund <jur.zikmund@gmail.com>
2025-07-08 17:53:35 +0200

    Fix memory leak in PKCS12 signing (#88)

--- src/vanillapdf/utils/pkcs12_key.cpp
+++ src/vanillapdf/utils/pkcs12_key.cpp
@@ -334,7 +334,7 @@ void PKCS12Key::PKCS12KeyImpl::SignInitialize(MessageDigestAlgorithm algorithm) 
     }
 
     if (p7bio != nullptr) {
-        BIO_free(p7bio);
+        BIO_free_all(p7bio);
         p7bio = nullptr;
     }
 
@@ -453,7 +453,7 @@ void PKCS12Key::PKCS12KeyImpl::SignCleanup() {
     }
 
     if (nullptr != p7bio) {
-        BIO_free(p7bio);
+        BIO_free_all(p7bio);
         p7bio = nullptr;
     }
 
