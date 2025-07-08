67f4c6e5ea9a2458147a5dacf9ce0bf7250ad487
Juraj Zikmund <jur.zikmund@gmail.com>
2025-07-08 14:43:18 +0200

    Add double-checked locking for OpenSSL cleanup

--- include/files.cmake
+++ include/files.cmake
@@ -18,6 +18,7 @@ set(VANILLAPDF_INCLUDE_UTILS_HEADERS
     "${VANILLAPDF_SOLUTION_SOURCE_DIR}/include/vanillapdf/utils/c_library_info.h"
     "${VANILLAPDF_SOLUTION_SOURCE_DIR}/include/vanillapdf/utils/c_license_info.h"
     "${VANILLAPDF_SOLUTION_SOURCE_DIR}/include/vanillapdf/utils/c_logging.h"
+    "${VANILLAPDF_SOLUTION_SOURCE_DIR}/include/vanillapdf/utils/c_misc_utils.h"
     "${VANILLAPDF_SOLUTION_SOURCE_DIR}/include/vanillapdf/utils/c_message_digest_algorithm.h"
     "${VANILLAPDF_SOLUTION_SOURCE_DIR}/include/vanillapdf/utils/c_output_stream.h"
     "${VANILLAPDF_SOLUTION_SOURCE_DIR}/include/vanillapdf/utils/c_memory_buffer_output_stream.h"
---
+++ include/vanillapdf/utils/c_misc_utils.h
@@ -0,0 +1,32 @@
+#ifndef _C_MISC_UTILS_H
+#define _C_MISC_UTILS_H
+
+#include "vanillapdf/c_export.h"
+#include "vanillapdf/c_values.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /**
+    * \file c_misc_utils.h
+    * \brief Initialization and cleanup helpers for miscellaneous utilities.
+    */
+
+    /**
+    * \memberof MiscUtils
+    * @{ */
+
+    /** \brief Initialize OpenSSL providers and algorithms. */
+    VANILLAPDF_API error_type CALLING_CONVENTION MiscUtils_InitializeOpenSSL();
+
+    /** \brief Cleanup OpenSSL providers and algorithms. */
+    VANILLAPDF_API error_type CALLING_CONVENTION MiscUtils_CleanupOpenSSL();
+
+    /** @} */
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* _C_MISC_UTILS_H */
